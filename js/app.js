8f596d31dbb73dbf8d0433370fe1dd5de32629b4
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-03-27 13:43:13 -0700

    add qyuestion 6

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ const answers = ['yes', 'no', 'y', 'n'];
 const questions = ['speak fluent Danish', 'like brussel sprouts', 'have a 1929 Harley Davidson', 'like to work all the live-long day',
   'like to hunt for meteorites'
 ];
+// get users name or if blank use ananoymous
 const userName = prompt('What is your name?');
 userName ? alert(`Hi ${userName}, let's play a guessing game about Ed`) : alert('Hi anonymous, let\'s play a guessing game about Ed');
 
+// prompt user until a yes/no response is entered
 for (let i = 0; i < questions.length; i++) {
   do {
     var response = prompt(`Does Ed ${questions[i]}? y/n`).toLowerCase();
@@ -15,7 +17,20 @@ for (let i = 0; i < questions.length; i++) {
     response === 'yes' || response === 'y' ? responseYes = true : responseYes = false;
   } while (!answers.includes(response));
 
+  // build a response message
   let negation = '';
   if ((responseYes && i % 2 !== 0) || (!responseYes && i % 2 !== 0)) negation = 'not';
   (responseYes && i % 2 === 0) || (!responseYes && i % 2 !== 0) ? alert(`That's correct, Ed does ${negation} ${questions[i]}`) : alert(`That's incorrect, Ed does ${negation} ${questions[i]}`);
 }
+
+// generate a random number and ask user to guess it
+let randomNumber = Math.floor(Math.random() * Math.floor(13)) + 1;
+console.log(randomNumber);
+let numberResponse;
+let j = 1;
+while (j < 5 && randomNumber !== numberResponse) {
+  numberResponse = parseInt(prompt(`Guess a random nuber between 1 and 13. You have 4 tries. Attempt ${j}`));
+  if (randomNumber === numberResponse) alert(`Nice! The random number was ${randomNumber}`);
+  j++;
+}
+if (randomNumber !== numberResponse) alert(`The random number was ${randomNumber} better luck next time.`);
